2212b91bfe193bf377743e4c12fce8cfa1c2b11a
jlcavazos84 <jlcavazos84@gmail.com>
2021-10-05 19:24:13 -0500

    Navmenu complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,29 @@ const Header = (props) => {
       <img src="/images/logo.svg" alt= "Disney+" /> 
     </Logo>
     <NavMenu>
-      <a href ='/home'>
+      <a href ="/home">
         <img src="/images/home-icon.svg" alt= "HOME"/>
         <span>HOME</span>
+      </a>
+      <a href ="/search">
+      <img src="/images/search-icon.svg" alt= "SEARCH"/>
+        <span>SEARCH</span>
+      </a>
+      <a href ="/watchlist">
+      <img src="/images/watchlist-icon.svg" alt= "WATCHLIST"/>
+        <span>WATCHLIST</span>
+      </a>
+      <a href ="/original">
+      <img src="/images/original-icon.svg" alt= "ORIGINALS"/>
+        <span>ORIGINALS</span>
+      </a>
+      <a href ="/movie">
+      <img src="/images/movie-icon.svg" alt= "MOVIES"/>
+        <span>MOVIES</span>
+      </a>
+      <a href ="/series">
+      <img src="/images/series-icon.svg" alt= "SERIES"/>
+        <span>SERIES</span>
       </a>
     </NavMenu>
   </Nav>
@@ -22,7 +42,7 @@ const Nav = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color:#090b13;
+  background-color: #090b13;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -49,7 +69,7 @@ const NavMenu = styled.div`
   display: flex;
   flex-flow: row nowrap;
   height: 100%;
-  justify-concent: flex-end;
+  justify-content: flex-end;
   margin: 0px;
   padding: 0px;
   position: relative;
@@ -69,46 +89,44 @@ a {
   }
 
   span {
-    color: rgb(249,249,249);
-    font-size: 13px;
-    letter-spacing: 1.42px;
-    line-height: 1.08;
-    padding: 2px;
-    white-space: nowrap;
-    position: relative;
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.08;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: "";
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
 
-    &:before {
-      background-color: rgb(249,249,249);
-      border-radius: 0px 0px 4px 4px;
-      bottom: -6px;
-      content: "";
-      height: 2px;
-      left: 0px;
-      opacity: 0;
-      position: absolute;
-      right: 0px;
-      transform-origin: left center;
-      transform: scaleX(0);
-      transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-      visibility: hidden;
-      width: auto;
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
     }
   }
-}
-
-&:hover {
-  span:before{
-    transform: scaleX(1);
-    visibility: visible;
-    opacity: 1 !important;
-  }
-
-}
-
-
-//   @media (max-width 768px){
-//   display: none;
-// }
+  /* @media (max-width: 768px) {
+    display: none;
+  } */
 `;
+
 
 export default Header;
